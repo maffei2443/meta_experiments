f27bc1be6a041f56c60ff4778b5dd9d4555fbf3a
Jader Martins <jadermcs94@gmail.com>
2019-12-09 15:19:58 -0300

    unused params

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #PBS -l select=1:ngpus=1
 #PBS -l walltime=3:00:00
 
-module load gcc
-module load python
-
 cd /home/$USER/meta_experiments/
 python metastream_clf/datawragling.py
 python metastream_clf/default-elec2.py --omega 300 --gamma 50 --initial 200 --target class --eval_metric acc > output.log
