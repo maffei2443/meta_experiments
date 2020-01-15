dafa51d0953508ee341940b33dbce11b25bc3677
Jader Martins <jadermcs94@gmail.com>
2020-01-15 16:01:39 -0300

    run file

--- run.sh
+++ run.sh
@@ -8,3 +8,11 @@ for i in 'elec2' 'flights' 'aws' 'intrusion';
     python -W ignore metastream/ms_incremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/$i/ > $i.output
     echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
 done
+echo non incremental
+for i in 'elec2' 'flights' 'aws' 'intrusion';
+  do
+    echo $i
+    echo start: $(date "+%y%m%d.%H%M%S.%3N")
+    python -W ignore metastream/ms_nonincremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/${i}_ninc/ > $i-ninc.output
+    echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
+done
