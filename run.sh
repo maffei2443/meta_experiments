de761303e16f5773fee6a200360a44a296768297
Jader Martins <jadermcs94@gmail.com>
2020-01-15 15:58:33 -0300

    run file

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #PBS -l select=1:ngpus=1
 #PBS -l walltime=3:00:00
 
-echo
-echo start: $(date "+%y%m%d.%H%M%S.%3N")
-echo
-
-cd /home/$USER/meta_experiments/
-# python metastream_clf/datawragling.py
-python metastream_clf/default-elec2.py --omega 300 --gamma 50 --initial 200 --target class --eval_metric acc > output.log
-
-echo
-echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
-echo
+for i in 'elec2' 'flights' 'aws' 'intrusion';
+  do
+    echo $i
+    echo start: $(date "+%y%m%d.%H%M%S.%3N")
+    python -W ignore metastream/ms_incremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/$i/ > $i.output
+    echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
+done
