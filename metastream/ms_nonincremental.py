2d6ded5f120e68ae78aa5d8f70fc361368bfd8df
Jader Martins <jadermcs94@gmail.com>
2020-01-19 06:49:54 -0300

    save extra data

--- metastream/ms_nonincremental.py
+++ metastream/ms_nonincremental.py
@@ -201,7 +201,10 @@ if __name__ == "__main__":
                               train_set=lgb.Dataset(metadf[-args.omega:],
                                                     metay[-args.omega:]))
 
+    dump(m_recommended, path + 'recommended.joblib')
+    dump(m_best, path + 'best.joblib')
     dump(m_diff, path + 'difference.joblib')
+    dump(metadf, path + 'metadf.joblib')
     print("Kappa: ", cohen_kappa_score(m_best, m_recommended))
     print("GMean: ", geometric_mean_score(m_best, m_recommended))
     print("Accuracy: ", accuracy_score(m_best, m_recommended))
