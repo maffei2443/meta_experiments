6180e3816def36f592469ae90034d4794faae2ad
Jader Martins <jadermcs94@gmail.com>
2020-02-03 16:45:30 -0300

    fix aws

--- metastream/aws/datawragling.py
+++ metastream/aws/datawragling.py
@@ -8,15 +8,13 @@ path = 'data/aws/'
 os.makedirs(path, mode=0o777, exist_ok=True)
 
 df = pd.read_csv(url).query("region=='eu-west-1a' and "+
-                                         "operating_system=='Linux/UNIX'")\
-                                  .drop(['region', 'operating_system'],axis=1)
-df = df.head(30000)
-df = df[df.instance_type.str.contains('r3.')].sort_values(['month', 'day',
-                                                           'hour', 'minute'])\
-                                             .reset_index(drop=True)
+                            "operating_system=='Linux/UNIX'"+
+                            "instance_type=='r3.large'")\
+                            .drop(['region', 'operating_system',
+                                   'instance_type'],axis=1)
+df = df.sort_values(['month', 'day', 'hour', 'minute']).reset_index(drop=True)
 df.price = (df.price > df.price.std()).astype(int)
 df.rename(columns={'price':'class'}, inplace=True)
-df = pd.get_dummies(df, columns=['instance_type'], drop_first=True)
 
 print(df.head())
 print(df.tail())
