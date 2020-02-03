cd17805dc4fd430d34733d0e6f2177951d7f0e8d
Jader Martins <jadermcs94@gmail.com>
2020-02-03 17:02:54 -0300

    fix aws filter wrong (prices from diff instances)

--- metastream/aws/datawragling.py
+++ metastream/aws/datawragling.py
@@ -7,10 +7,10 @@ path = 'data/aws/'
 
 os.makedirs(path, mode=0o777, exist_ok=True)
 
-df = pd.read_csv(url).query("region=='eu-west-1a' and "+
-                            "operating_system=='Linux/UNIX'"+
+df = pd.read_csv(url).query("region=='eu-west-1a' and "
+                            "operating_system=='Linux/UNIX'"
                             "instance_type=='r3.large'")\
-                            .drop(['region', 'operating_system',
+                     .drop(['region', 'operating_system',
                                    'instance_type'],axis=1)
 df = df.sort_values(['month', 'day', 'hour', 'minute']).reset_index(drop=True)
 df.price = (df.price > df.price.std()).astype(int)
