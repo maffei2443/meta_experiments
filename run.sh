84beae48fbe0a26d25350a7aa7b66afa2d168231
Jader Martins <jadermcs94@gmail.com>
2020-03-09 20:19:43 -0300

    nohup sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ for i in 'elec2' 'powersupply' 'covtype';
   do
     echo $i
     echo start: $(date "+%y%m%d.%H%M%S.%3N")
-    python -W ignore metastream/ms_incremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/$i/ > $i.output &
+    nohup python metastream/ms_incremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/$i/ > $i.output &
     echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
 done
 echo non incremental
@@ -13,6 +13,6 @@ for i in 'elec2' 'powersupply' 'covtype';
   do
     echo $i
     echo start: $(date "+%y%m%d.%H%M%S.%3N")
-    python -W ignore metastream/ms_nonincremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/${i}_ninc/ > $i-ninc.output &
+    nohup python metastream/ms_nonincremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/${i}_ninc/ > $i-ninc.output &
     echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
 done
