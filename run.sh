b5f83ac69902cd1f5808b4937604bc97b45f9f8b
Jader Martins <jadermcs94@gmail.com>
2020-06-28 20:16:17 -0300

    calc kappa removing ties

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ for i in 'elec2' 'powersupply' 'covtype';
     nohup python metastream/ms_incremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/$i/ > $i.output &
     echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
 done
-echo non incremental
-for i in 'elec2' 'powersupply' 'covtype';
-  do
-    echo $i
-    echo start: $(date "+%y%m%d.%H%M%S.%3N")
-    nohup python metastream/ms_nonincremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/${i}_ninc/ > $i-ninc.output &
-    echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
-done
+#echo non incremental
+#for i in 'elec2' 'powersupply' 'covtype';
+#  do
+#    echo $i
+#    echo start: $(date "+%y%m%d.%H%M%S.%3N")
+#    nohup python metastream/ms_nonincremental.py --omega 300 --gamma 10 --initial 300 --target class --eval_metric acc --path data/${i}_ninc/ > $i-ninc.output &
+#    echo stop:  $(date "+%y%m%d.%H%M%S.%3N")
+#done
